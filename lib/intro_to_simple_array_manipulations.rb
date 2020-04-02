d15e2b89b5f6cb780fc5a4132c384b759c2724e0
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-04-02 00:32:26 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(4, element) # use the number 4 here as it is the 4th index to be deleted
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
