00531bbf3abc512a51fa0484c7f82d8a912b58d5
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-04-02 00:32:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,6 +18,6 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, Integer)
-  array.delete_at(2,Integer)
+def using_delete_at(array, integer)
+  array.delete_at(Integer)
 end
