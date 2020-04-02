ec4320c97f1c9b81ba6f6f9e322629aa89af2754
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-04-02 00:22:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,11 @@
 def using_concat(array1, array2)
   array1.concat(array2)
 end
-#def using_insert(array, element)
-  #array.insert(element)
-#end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
 def using_uniq(array)
   array.uniq
 end
@@ -15,3 +17,7 @@ end
 def using_delete(array, string)
   array.delete(string)
 end
+
+#def using_delete_at(array, Integer)
+  #array.delete_at(Integer)
+#end
